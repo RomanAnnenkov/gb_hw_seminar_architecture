623a86e95353fbf71295e9d7e7c29ae9560570be
Roman Annenkov <roman.annenkov@gmail.com>
2023-09-24 19:01:00 +0300

    added observer example

--- src/main/java/seminarTwo/Main.java
+++ src/main/java/seminarTwo/Main.java
@@ -3,6 +3,9 @@ package seminarTwo;
 import seminarTwo.decorator.Drawable;
 import seminarTwo.decorator.GreenDrawableDecorator;
 import seminarTwo.decorator.Triangle;
+import seminarTwo.observer.Publisher;
+import seminarTwo.observer.SubscriberOne;
+import seminarTwo.observer.SubscriberTwo;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +14,14 @@ public class Main {
         Drawable greenTriangle = new GreenDrawableDecorator(new Triangle());
         greenTriangle.draw();
 
+        SubscriberOne subscriberOne = new SubscriberOne();
+        SubscriberTwo subscriberTwo = new SubscriberTwo();
+        Publisher publisher = new Publisher();
+
+        publisher.attach(subscriberOne);
+        publisher.attach(subscriberTwo);
+
+        publisher.notify("test message");
         
     }
 }
